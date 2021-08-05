24dbcb637c9e90fad72fa00dc59c232efa02e708
Crowdbotics <team@crowdbotics.com>
2021-08-05 12:50:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "patient_credit_29504Identifier",
+  url: "https://patient_credit_29504Identifier.botics.co",
+  api: "https://patient_credit_29504Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
